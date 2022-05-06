f52133b52b742a03dfd842bb5dabf989d665878d
Kayode Adeniyi <adeniyikayode22@gmail.com>
2022-05-06 16:36:34 +0100

    setting up

--- main.tf
+++ main.tf
@@ -1,12 +1,119 @@
 // this is the provider
 provider "aws" {
-  region     = "us-east-1"
-  access_key = "AKIAYUKYHRRJNNXJFVNG"
-  secret_key = "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"
+  region     = "us-east-2"
+  access_key = "AKIAYUKYHRRJOPR6DFZJ"
+  secret_key = "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"
 }
 
-// create instances
-resource "aws_instance" "my-first-try" {
-  ami           = "ami-015c25ad8763b2f11"
-  instance_type = "t2.micro"
+## 1. Create vpc
+resource "aws_vpc" "prod-vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+      Name = "production"
+  }
 }
+
+## 2. Create Internet Gateway
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.prod-vpc.id
+
+}
+
+## 3. Create Custom Route Table
+resource "aws_route_table" "prod-route-table" {
+  vpc_id = aws_vpc.prod-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  route {
+    ipv6_cidr_block        = "::/0"
+    egress_only_gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+      Name = "Prod"
+  }
+}
+
+## 4. Create a Subnet
+resource "aws_subnet" "subnet-1" {
+  vpc_id = aws_internet_gateway.gw.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-2a"
+
+  tags = {
+    "Name" = "prod-subnet"
+  }
+}
+
+## 5. Associate Subnet with the Route Table
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.subnet-1.id
+  route_table_id = aws_route_table.prod-route-table.id
+}
+
+## 6. Create a security Group to allow port 22,80,443
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_web_traffic"
+  description = "Allow web inbound traffic"
+  vpc_id      = aws_vpc.prod-vpc.id
+
+  ingress {
+    description      = "HTTPS"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["0.0.0.0/0"]
+  }
+
+    ingress {
+    description      = "HTTPS"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["0.0.0.0/0"]
+  }
+
+    ingress {
+    description      = "HTTPS"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_web"
+  }
+}
+
+## 7. Create a network interface with an ip in the subnet that was created in 4
+resource "aws_network_interface" "web-server" {
+  subnet_id       = aws_subnet.subnet-1.id
+  private_ips     = ["10.0.0.50"]
+  security_groups = [aws_security_group.web.id]
+
+}
+
+## 8. Assign an elastic IP to the network interface created in step 7
+resource "aws_eip" "one" {
+  vpc                       = true
+  network_interface         = aws_network_interface.web-server.id
+  associate_with_private_ip = "10.0.1.50"
+}
+
+## 9. Create Ubuntu server and install/enable apache2
