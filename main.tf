1216eb6e7e733195fe39f33e77be454cfcd58971
Kayode Adeniyi <adeniyikayode22@gmail.com>
2022-05-06 10:33:37 +0100

    created instance

--- main.tf
+++ main.tf
@@ -5,4 +5,8 @@ provider "aws" {
   secret_key = "VDn9yAPB2fHdfiBHUbQufXwjHLs3aSeK7Lwr54Wc"
 }
 
-// 
+// create instances
+resource "aws_instance" "my-first-try" {
+  ami           = "ami-015c25ad8763b2f11"
+  instance_type = "t2.micro"
+}
